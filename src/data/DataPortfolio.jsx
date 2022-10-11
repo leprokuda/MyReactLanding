12e1a44091c17b82486d105c7b151b9ef3a10840
leprokuda <leprokuda@gmail.com>
2022-10-11 18:42:59 +0600

    Осталось доделать Контакты и Футер

---
+++ src/data/DataPortfolio.jsx
@@ -0,0 +1,75 @@
+import IMG1 from '../assets/img/portfolio1.png'
+import IMG2 from '../assets/img/portfolio2.png'
+import IMG3 from '../assets/img/portfolio3.png'
+import IMG4 from '../assets/img/portfolio4.png'
+import IMG5 from '../assets/img/portfolio5.png'
+import IMG6 from '../assets/img/portfolio6.png'
+import IMG7 from '../assets/img/portfolio7.png'
+import IMG8 from '../assets/img/portfolio8.png'
+import IMG9 from '../assets/img/portfolio9.png'
+
+export const DataPortfolio = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Landing page layout with Figma',
+    github: 'https://github.com/leprokuda/layoutOne',
+    demo: 'https://leprokuda.github.io/layoutOne/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'First landing page for myself',
+    github: 'https://github.com/leprokuda/leprodev',
+    demo: 'https://leprokuda.github.io/leprodev/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Website page for the company "EKON URFO"',
+    github: 'https://github.com/leprokuda/layoutTwo',
+    demo: 'https://leprokuda.github.io/layoutTwo/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Free style test site "LevelUp"',
+    github: 'https://github.com/leprokuda/testLevelUp',
+    demo: 'https://leprokuda.github.io/testLevelUp/'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Bright Task Tracker Infinity+',
+    github: 'https://github.com/leprokuda/taskboard',
+    demo: 'https://leprokuda.github.io/taskboard/ '
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Landing page layout with Figma',
+    github: 'https://github.com/leprokuda/UltimateForm',
+    demo: 'https://leprokuda.github.io/UltimateForm/'
+  },
+  {
+    id: 7,
+    image: IMG7,
+    title: 'Table with sorting, filtering, searching and pagination (Click on pagination)',
+    github: 'https://github.com/leprokuda/table-task',
+    demo: 'https://leprokuda.github.io/table-task/'
+  },
+  {
+    id: 8,
+    image: IMG8,
+    title: 'The game "Find the word" Wordle',
+    github: 'https://github.com/leprokuda/Wordle',
+    demo: 'https://leprokuda.github.io/Wordle/'
+  },
+  {
+    id: 9,
+    image: IMG9,
+    title: 'Website for company "Happy People" on Tilda',
+    github: 'https://github.com/leprokuda/happy-people-banners',
+    demo: 'http://hp-banners.ru/new'
+  },
+]
---
+++ src/data/DataTestimonials.jsx
@@ -0,0 +1,20 @@
+import AVTR1 from '../assets/img/avatar1.png'
+import AVTR2 from '../assets/img/avatar1.png'
+import AVTR3 from '../assets/img/avatar1.png'
+
+export const DataTestimonials = [
+  {
+    avatar: AVTR1,
+    name: 'Jhon Brasco',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur enim fugit, ipsa ipsam maxime quisquam quos? Beatae eius nihil tenetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ducimus harum hic illo maxime nostrum obcaecati quidem repellendus totam? Quis.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Dmitriy Welter',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur enim fugit, ipsa ipsam maxime quisquam quos? Beatae eius nihil tenetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ducimus harum hic illo maxime nostrum obcaecati quidem repellendus totam? Quis.'
+  },  {
+    avatar: AVTR3,
+    name: 'Evgeniy Kravchenko',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur enim fugit, ipsa ipsam maxime quisquam quos? Beatae eius nihil tenetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ducimus harum hic illo maxime nostrum obcaecati quidem repellendus totam? Quis.'
+  },
+]
