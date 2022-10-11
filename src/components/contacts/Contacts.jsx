6f4c6b1292c05b8d0e764586ea6f3f8b599d8e01
leprokuda <leprokuda@gmail.com>
2022-10-11 21:54:01 +0600

    Весь сайт готов, необходимо заменить рыбу на контент

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import {React, useRef} from 'react'
 import {MdOutlineMail} from 'react-icons/md'
 import {TbBrandTelegram} from 'react-icons/tb'
 import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from 'emailjs-com';
 
 import './contacts.css'
 
 const Contacts = () => {
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fzfepvr', 'template_t8qrz4i', form.current, 'yrZtM9ur4nVC8J2nQ')
+
+    e.target.reset()
+  };
+
   return (
     <section id='contacts'>
       <h5>Get In Touch</h5>
@@ -14,27 +24,32 @@ const Contacts = () => {
       <div className="container contacts__container">
         <div className="contacts__options">
           <article className="contacts__option">
-            <MdOutlineMail/>
+            <MdOutlineMail className="contacts__option-icon"/>
             <h4>Email</h4>
             <h5>leprokuda@yandex.ru</h5>
             <a href="mailto:leprokuda@yandex.ru" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className="contacts__option">
-            <TbBrandTelegram/>
+            <TbBrandTelegram className="contacts__option-icon"/>
             <h4>Telegram</h4>
             <h5>@leprokuda</h5>
             <a href="https://t.me/leprokuda" target="_blank">Send a message</a>
           </article>
 
           <article className="contacts__option">
-            <BsWhatsapp/>
+            <BsWhatsapp className="contacts__option-icon"/>
             <h4>WhatsApp</h4>
             <h5>+7(996)543-51-91</h5>
             <a href="https://api.whatsapp.com/send?phone=79965435191&text=" target="_blank">Send a message</a>
           </article>
         </div>
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required/>
+          <input type="email" name="email" placeholder="Your Email" required/>
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
       </div>
     </section>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Icon24LogoVk} from "@vkontakte/icons";
+import {FaGithub} from "react-icons/fa";
+import {BsInstagram} from "react-icons/bs";
+
 import './footer.css'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo"></a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contacts">Contacts</a></li>
+      </ul>
+      
+      <div className="footer__socials">
+        <a href="https://vk.com/leprokuda" target="_blank"><Icon24LogoVk width={21.45} height={21.45}/></a>
+        <a href="https://github.com/leprokuda" target="_blank"><FaGithub/></a>
+        <a href="https://instagram.com/leprokuda" target="_blank"><BsInstagram/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; leprokuda All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
